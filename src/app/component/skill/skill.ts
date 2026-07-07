e28593e9025d41b2e9800361b92696a6846d8a2e
reesealexandercode-alt <reesealexandercode@gmail.com>
2026-07-07 08:43:17 -0400

    updated skills, achievements, experience

--- src/app/component/skill/skill.ts
+++ src/app/component/skill/skill.ts
@@ -1,9 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-skill',
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './skill.html',
   styleUrl: './skill.css',
 })
-export class Skill {}
+export class Skill {
+  readonly = true;
+  csharpMax = 5;
+  csharpRate = 4;
+  sqlMax = 5;
+  sqlRate = 5;
+  netMax = 5;
+  netRate = 3;
+  angularMax = 5;
+  angularRate = 4;
+  htmlMax = 5;
+  htmlRate = 5;
+  cssMax = 5;
+  cssRate = 5;
+
+  getStars(rating: number, max: number): string[] {
+    return Array.from({ length: max }, (_, index) => (index < rating ? 'filled' : 'empty'));
+  }
+}
